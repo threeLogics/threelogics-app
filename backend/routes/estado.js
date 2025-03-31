c0065447601ca1a68ce43cdd45c98d48b9e72125
danielramo-dev <danielramo2305@gmail.com>
2025-03-31 09:52:37 +0200

    cambio en estado.js

--- backend/routes/estado.js
+++ backend/routes/estado.js
@@ -54,8 +54,8 @@ router.get("/", async (req, res) => {
 
     const inicioDB = Date.now();
     const { error: dbError } = await supabase
-      .from("productos") // ✅ una tabla válida
-      .select("id")
+    .from("auth.users") 
+    .select("id")
       .limit(1);
 
     if (dbError) {
